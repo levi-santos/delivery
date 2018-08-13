cd3a8f8df872ea2efff11be1d8ea232c2e176834
Levi Santos <levisantos.cs@gmail.com>
2018-08-13 01:14:56 -0300

    Atualização inicial

--- footer.php
+++ footer.php
@@ -1,4 +1,9 @@
 
+<footer class="footer">
+    <div class="shell">
 
+    </div>
+<h4>Chapolin!!!! Não contavam com minha Astúcia ??? </h4>
+</footer>
 </body>
 </html>
--- header.php
+++ header.php
@@ -3,12 +3,39 @@
 
 
 ?>
-
+<!DOCTYPE <!DOCTYPE html>
 <html>
 <head>
-<title> Delivery Franca Anderson!!!!  </title>
-<meta charset="utf-8">
-<link rel="stylesheet" type="text/css" href="lib/css/estilo.css" >
-<link href="lib/bs4/css/bootstrap.css" rel="stylesheet" media="screen">
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Delivery Anderson Franca Exemplo1</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="lib/css/estilo.css" >
+    <link href="lib/bs4/css/bootstrap.css" rel="stylesheet" media="screen">
+    <script src="lib/js/main.js"></script>
 </head>
+<body>
+<header class="header" style="top: 0px;">
+<div class="shell clearfix">
+<button class="first hide" type="button"><span class="burger-menu">Navegação</span></button>
+<div class="logo">
+    <a href="/" title="ichapo">Chapolin</a>
+</div>
+<nav class="navigation">
+<ul>
+<li><a href="#" title="Sobre iChapo"> Sobre o Chapolin </a>
+<ul class="dropdown sobre">
+<li class="about-link"><a href="#" title="Quem somos">Quem somos!</a> </li>
+<li class="terms-link"><a href="#" tilte="Termos e condições de uso">Termo e condições de uso</a> </li>
+<li class="privancy-link"><a href="#" title="Privacidade"> Privacidade</a></li>
+</ul>
+</li>
+<li> </li>
+<li class="contact-section"> </li>
+<li> </li>
+</ul>
+</nav>
+</div>
+</header>
+
 
